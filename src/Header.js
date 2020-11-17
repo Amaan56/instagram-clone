cf757adb03e88c76209fd549a5723138f02111c2
Amaan56 <shaikhamaan257@gmail.com>
2020-11-17 07:57:53 +0530

    Header logo added

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__container">
+        <img
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="Instagram Logo"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
