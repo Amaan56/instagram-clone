c5c8fbb94c6c8e0380ef50224e5f492924783a25
Amaan56 <shaikhamaan257@gmail.com>
2020-11-21 01:32:30 +0530

    Like section

--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import './Post.css';
 import Avatar from '@material-ui/core/Avatar';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
@@ -6,7 +7,16 @@ import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import ShareIcon from '@material-ui/icons/Share';
 import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+
+const useStyles = makeStyles((theme) => ({
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+}));
+
 function Post() {
+  const classes = useStyles();
   return (
     <div className="post">
       <div className="post__header">
@@ -34,6 +44,17 @@ function Post() {
           </div>
           <div className="post__footer__icons__right">
             <BookmarkBorderIcon />
+          </div>
+        </div>
+        <div className="post__footer_likeSection">
+          <Avatar
+            className={classes.small}
+            alt="Amaan Shaikh"
+            src="https://instagram.fbom2-1.fna.fbcdn.net/v/t51.2885-19/s150x150/83639244_187705492295818_9221091469350141952_n.jpg?_nc_ht=instagram.fbom2-1.fna.fbcdn.net&_nc_ohc=ma5Yw1PI8_IAX-b1V4F&_nc_tp=25&oh=212594a42db841ea827f21820a06e443&oe=5FDBE121"
+          />
+          <div style={{ fontSize: '14px', margin: '10px' }}>
+            Liked By <span style={{ fontWeight: 500 }}>akshata_pathare </span>
+            and <span style={{ fontWeight: 500 }}>441 others</span>
           </div>
         </div>
       </div>
