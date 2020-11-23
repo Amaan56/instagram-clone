2373f3a09c27533311015122855fec48d8bdd640
Amaan56 <shaikhamaan257@gmail.com>
2020-11-23 23:01:50 +0530

    Backdrop component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header';
 import Main from './Main';
+import Backdrop from './Backdrop';
 
 function App() {
   return (
     <div className="app">
+      <Backdrop />
       <Header />
       <Main />
     </div>
